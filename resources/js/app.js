78aa1be16f2ee3baf715e43c1d56a5b51cb11b7b
KrisnaMahardika <gedekrisna119@gmail.com>
2026-02-18 15:36:41 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import './landing_page/app.js';
 // import { runLandingPageModules } from './admin/landing_page/app.js';
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Jalankan semua logic Landing Page
-    runLandingPageModules();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Jalankan semua logic Landing Page
+//     runLandingPageModules();
+// });
